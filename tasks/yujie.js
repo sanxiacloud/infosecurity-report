4b8418cde8df9ec1ec19e72d685e3fa875df5123
idlej <muxiatianzhong@gmail.com>
2019-09-17 17:25:14 +0800

    修改御界通过elasticsearch获取数据

--- tasks/yujie.js
+++ tasks/yujie.js
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 'use strict';
 
 const moment = require('moment');
@@ -6,36 +7,93 @@ const { isServerIP } = require('../libs/ip-filter');
 let timer;
 
 async function task(earliest, tools) {
-  let alerts = [];
+  let response;
+  let latest = earliest;
+  const hits = [];
   try {
-    alerts = await tools.splunk.search(`index="user_index_yujie" message_type=sec
-      asset IN ("*10.19.213.*", "*10.19.214.*", "*10.19.248.*", "*10.19.249.*", "*10.19.250.*", "*10.19.251.*", "*10.19.252.*", "*10.19.253.*", "*10.19.254.*", "*10.19.255.*", "*10.19.110.*", "*10.19.136.*", "*10.19.137.*", "*10.19.138.*", "*10.19.139.*", "*10.19.140.*", "*10.19.141.*", "*10.19.142.*", "*10.19.143.*", "*10.19.144.*", "*10.19.145.*", "*10.19.146.*", "*10.19.147.*", "*10.19.148.*", "*10.19.149.*", "*10.19.150.*", "*10.19.151.*", "*10.19.195.*", "*10.27.*.*", "*10.28.*.*", "*10.29.*.*", "*10.30.1.*", "*10.33.*.*", "*10.34.*.*", "*10.35.*.*", "*172.22.224.*", "*172.22.248.*")`
-    , [earliest.format()]);
+    response = await tools.esClient.search({
+      index: 'syslog-search',
+      body: {
+        query: {
+          bool: {
+            filter: [
+              {
+                bool: {
+                  should: [
+                    {term: {'yujie.asset': '10.19.213.0/24'}},
+                    {term: {'yujie.asset': '10.19.214.0/24'}},
+                    {term: {'yujie.asset': '10.19.248.0/21'}},
+                    {term: {'yujie.asset': '10.19.110.0/24'}},
+                    {term: {'yujie.asset': '10.19.136.0/21'}},
+                    {term: {'yujie.asset': '10.19.144.0/21'}},
+                    {term: {'yujie.asset': '10.19.195.0/24'}},
+                    {term: {'yujie.asset': '10.27.0.0/16'}},
+                    {term: {'yujie.asset': '10.28.0.0/16'}},
+                    {term: {'yujie.asset': '10.29.0.0/16'}},
+                    {term: {'yujie.asset': '10.30.1.0/24'}},
+                    {term: {'yujie.asset': '10.33.0.0/16'}},
+                    {term: {'yujie.asset': '10.34.0.0/16'}},
+                    {term: {'yujie.asset': '10.35.0.0/16'}},
+                    {term: {'yujie.asset': '172.22.224.0/24'}},
+                    {term: {'yujie.asset': '172.22.248.0/24'}},
+                  ],
+                },
+              },
+              {
+                range: {
+                  'syslog.timestamp': {
+                    gt: earliest.format(),
+                  },
+                },
+              },
+              {
+                'term': {
+                  'yujie.message_type': 'sec',
+                },
+              },
+            ],
+            must_not: [
+              {
+                term: {'yujie.type': '入侵感知'},
+              },
+            ],
+          },
+        },
+      },
+      scroll: '10s',
+      size: 1000,
+    });
   } catch (err) {
     console.log(err);
+    return;
   }
-  let latest = earliest;
-  alerts.forEach((alert)=>{
-    const detail = alert.result._raw.detail;
-    const current = moment(detail.time, 'YYYY-MM-DD HH:mm:ss.SSS');
-    if (current.isAfter(latest)) {
-      latest = current.add(1, 's');
-    }
-    if (detail.type == '入侵感知') {
+  hits.push(...response.body.hits.hits);
+  while (hits.length < response.body.hits.total.value) {
+    try {
+      response = await tools.esClient.scroll({
+        scroll_id: response.body._scroll_id,
+        scroll: '10s',
+      });
+    } catch (err) {
+      console.log(err);
       return;
     }
+    hits.push(...response.body.hits.hits);
+  }
+  hits.map( (hit)=> {
+    const raw = hit._source;
+    const current = moment(raw.syslog.timestamp);
+    if (current.isAfter(latest)) {
+      latest = current;
+    }
     const data = {
-      time: detail.time,
-      attackStage: detail.attackStage,
-      type: detail.type,
+      time: raw.yujie.time,
+      attackStage: raw.yujie.attackStage,
+      type: raw.yujie.type,
     };
-    const ips = alert.result.asset.split(';');
-    console.log(ips);
-    ips.forEach(async (ip) => {
-      // 没有查到hostinfo时考虑使用默认host，顺便提示存在未监控主机
+    raw.yujie.asset.forEach(async (ip) => {
       console.log(ip);
-      if (!isServerIP(ip))
-      {
+      if (!isServerIP(ip)) {
         console.log(`no server ip address ${ip}`);
         return;
       }
